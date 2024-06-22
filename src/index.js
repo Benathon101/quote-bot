f0d11966872396bd79a77c13083f00ace570a14c
ben_gorman <bengorman1994@gmail.com>
2024-06-22 12:28:31 +1000

    retain?

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // Require the necessary discord.js classes
 import 'dotenv/config'
-import { Client, Events, GatewayIntentBits } from 'discord.js'
+import fs from "node:fs"
+import path from "node:path"
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js'
 const token = process.env.DISCORD_TOKEN
 
 // Create a new client instance
@@ -15,3 +17,21 @@ client.once(Events.ClientReady, (readyClient) => {
 
 // Log in to Discord with your client's token
 client.login(token)
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
